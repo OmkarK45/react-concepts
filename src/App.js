fda256256adecd24da18acd19c673acffb59c1a8
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 18:27:34 +0530

    Added Components Counter1 and Counter2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import Movie from "./hoc/Movie";
+import Counter1 from "./Hooks/Counter";
+import Counter2 from './Hooks/CounterUsingSFC';
 
 function App() {
-  return <Movie />;
+  return (
+    <React.Fragment>
+      <Movie />
+      <Counter1 />
+      <Counter2/>
+    </React.Fragment>
+  );
 }
 
 export default App;
