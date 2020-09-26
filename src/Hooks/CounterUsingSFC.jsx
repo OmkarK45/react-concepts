f2a0dca2302649a5ba5a550ff5243675713de298
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 20:03:54 +0530

    Implemented simple snippet using useEffect()

--- src/Hooks/CounterUsingSFC.jsx
+++ src/Hooks/CounterUsingSFC.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter2 = (props) => {
   // Initial counter value is set to 0 it will return an array
@@ -10,13 +10,26 @@ const Counter2 = (props) => {
   const setCount = array[1];
   //   Short way of doing above 3 lines is by using array destructuring
   // const [count, setCount] = useState(0)
-    const [name, setName]=useState('')
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    document.title = `${name} has clicked ${count} times !`;
+    return () => {
+      //   write cleanup code.. anything present in component will unmount
+      console.log("cleanup");
+    };
+  });
+
+  //   component will unmount
+
   return (
     <div>
       <React.Fragment>
         <h3>Count using SFC</h3>
-          <input type="text" onChange={e=> setName(e.target.value)}/>
-        <div>{name} has clicked : {count} times</div>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+        <div>
+          {name} has clicked : {count} times
+        </div>
         <button onClick={() => setCount(count + 1)}>Increase Count</button>
       </React.Fragment>
     </div>
