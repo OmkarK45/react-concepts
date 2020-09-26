f2c74e4ac53b306a35058cb86b51a45a2562f6f6
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 20:58:47 +0530

    Added User render component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Movie from "./hoc/Movie";
 import Counter1 from "./Hooks/Counter";
 import Counter2 from './Hooks/CounterUsingSFC';
+import Users from "./Hooks/Users";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Movie />
       <Counter1 />
       <Counter2/>
+      <Users/>
     </React.Fragment>
   );
 }
