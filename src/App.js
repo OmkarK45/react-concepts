57f53bde45e83bf6017685eb258509a4717d9831
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 21:56:11 +0530

    Commented some code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Movie from "./hoc/Movie";
 import Counter1 from "./Hooks/Counter";
-import Counter2 from './Hooks/CounterUsingSFC';
+import Counter2 from "./Hooks/CounterUsingSFC";
 import Users from "./Hooks/Users";
-
-function App() {
-  return (
-    <React.Fragment>
-      <Movie />
-      <Counter1 />
-      <Counter2/>
-      <Users/>
-    </React.Fragment>
-  );
+import MoviePage from "./context/MoviePage";
+import UserContext from "./context/userContext";
+class App extends Component {
+  state = { currentUser: { name: "Omkar" } };
+  render() {
+    return (
+      <div>
+        <UserContext.Provider value={this.state.currentUser}>
+          <React.Fragment>
+            {/* <Movie />
+          <Counter1 />
+          <Counter2 />
+          <Users /> */}
+            <MoviePage />
+          </React.Fragment>
+        </UserContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
