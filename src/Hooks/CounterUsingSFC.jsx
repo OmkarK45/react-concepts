2beef2a305f19f1d73a3540a1957de540e92fec9
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 19:16:51 +0530

    Showed how exactly hooks work

--- src/Hooks/CounterUsingSFC.jsx
+++ src/Hooks/CounterUsingSFC.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 
-const Counter2=(props)=>{
-  const [count, setCount] = useState(0);
+const Counter2 = (props) => {
+  // Initial counter value is set to 0 it will return an array
+  const array = useState(0);
+  const count = array[0]; // this.state.count
+
+  // second item returned from useState is a function which will update the first value , count here
+  // this.setState()
+  const setCount = array[1];
+  //   Short way of doing above 3 lines is by using array destructuring
+  // const [count, setCount] = useState(0)
+    const [name, setName]=useState('')
   return (
     <div>
       <React.Fragment>
         <h3>Count using SFC</h3>
-        <div>Count : {count}</div>
-        <button onClick={()=>setCount(count+1)}>Increase Count</button>
+          <input type="text" onChange={e=> setName(e.target.value)}/>
+        <div>{name} has clicked : {count} times</div>
+        <button onClick={() => setCount(count + 1)}>Increase Count</button>
       </React.Fragment>
     </div>
   );
