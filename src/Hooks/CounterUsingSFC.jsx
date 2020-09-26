2fbf907485a155cbf20b89ed73d6744b70230a0e
Omkar Kulkarni <omkarak45@gmail.com>
2020-09-26 20:56:31 +0530

    Used custom hook

--- src/Hooks/CounterUsingSFC.jsx
+++ src/Hooks/CounterUsingSFC.jsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import useDocumentTitle from "./useDocumentTitle";
 
 const Counter2 = (props) => {
-  // Initial counter value is set to 0 it will return an array
-  const array = useState(0);
-  const count = array[0]; // this.state.count
-
-  // second item returned from useState is a function which will update the first value , count here
-  // this.setState()
-  const setCount = array[1];
-  //   Short way of doing above 3 lines is by using array destructuring
-  // const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [name, setName] = useState("");
-
-  useEffect(() => {
-    document.title = `${name} has clicked ${count} times !`;
-    return () => {
-      //   write cleanup code.. anything present in component will unmount
-      console.log("cleanup");
-    };
-  });
-
-  //   component will unmount
+//   Custom Hook
+  useDocumentTitle(`${name} has clicked ${count} times`);
 
   return (
     <div>
